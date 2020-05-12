5a598e79f9adfbae6805729a61e80741bd3f9bf6
The Spine <dtorkelson@gmail.com>
2020-05-12 18:18:59 -0400

    remove sudo from running logstash

--- Montgomery_push_index.sh
+++ Montgomery_push_index.sh
@@ -1,4 +1,8 @@
+# replace the template
 curl -X PUT \
 	"192.168.1.145:9200/_template/crime_montgomery?pretty" \
 	-H 'Content-Type: application/json' \
 	--data-binary "@Montgomery_index_template.json"
+
+# Use Logstash to index data
+$HOME/Elastic/logstash-7.6.2/bin/logstash -f Montgomery_logstash.conf

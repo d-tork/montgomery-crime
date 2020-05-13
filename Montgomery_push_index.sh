8f7eec046d82dd5ef65e4dd578bf7ff0be655c8a
The Spine <dtorkelson@gmail.com>
2020-05-13 14:13:25 -0400

    handle template refresh through API

--- Montgomery_push_index.sh
+++ Montgomery_push_index.sh
@@ -2,10 +2,10 @@
 curl -X DELETE "192.168.1.145:9200/crime_montgomery_1?pretty"
 
 # replace the template
-#curl -X PUT \
-#	"192.168.1.145:9200/_template/crime_montgomery?pretty" \
-#	-H 'Content-Type: application/json' \
-#	--data-binary "@Montgomery_index_template.json"
+curl -X PUT \
+	"192.168.1.145:9200/_template/crime_montgomery?pretty" \
+	-H 'Content-Type: application/json' \
+	--data-binary "@Montgomery_index_template.json"
 
 # Use Logstash to index data
 sudo $HOME/Elastic/logstash-7.6.2/bin/logstash -f Montgomery_logstash.conf
